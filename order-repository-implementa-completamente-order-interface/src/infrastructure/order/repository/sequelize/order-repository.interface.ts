9d4e5eb9547b68f9e16c1ae7e1f54c1e651a37d3
AlexandreNoguez <alexandrenoguez_rs@yahoo.com.br>
2025-09-21 19:45:29 -0300

    refact: add checkout module

--- order-repository-implementa-completamente-order-interface/src/infrastructure/order/repository/sequelize/order-repository.interface.ts
+++
@@ -1,5 +0,0 @@
-import Order from "../../../../domain/order/order";
-import RepositoryInterface from "../../../../domain/shared/repository/repository-interface";
-
-export default interface OrderRepositoryInterface
-  extends RepositoryInterface<Order> {}
